93b069978a1b755bb6f04f6c63fcd002be9e250b
Aaron To <aaronto0705@gmail.com>
2024-04-07 12:43:30 -0400

    Added spotify login

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,25 @@
-import { Text, Pressable, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, Pressable, SafeAreaView, Linking } from 'react-native'
+import React from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+    
+
+    return (
+        <SafeAreaView>
+            <Text>HomeScreen</Text>
+            <Pressable onPress={() => navigation.navigate('Login')}>
+                <Text>Logout</Text>
+            </Pressable>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})import { Text, Pressable, SafeAreaView } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 
