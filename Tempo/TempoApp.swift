1d4f56c08843aeda4371523bc8b72157bfbb5d74
Carina Sanborn <czsanbor@andrew.cmu.edu>
2024-04-07 09:23:35 -0400

    Initial Commit

---
+++ Tempo/TempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoApp.swift
+//  Tempo
+//
+//  Created by Carina Sanborn on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
