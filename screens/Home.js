fafcc8c5004826067e51c72e4fcb8613ad148b0f
Aaron To <aaronto0705@gmail.com>
2024-04-17 00:55:00 -0400

    Storing all spotify data in firebase

--- screens/Home.js
+++ screens/Home.js
@@ -3,7 +3,7 @@ import { View, FlatList, TouchableOpacity, StyleSheet, Text, Pressable } from 'r
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
 
 // Initialize Firebase
 var firebaseConfig = {
@@ -12,16 +12,21 @@ var firebaseConfig = {
     storageBucket: "tempo-9e317.appspot.com",
     messagingSenderId: "85989036364",
     appId: "1:85989036364:ios:345da87677feedd4133fe9"
-  };
-  
+};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const db = getDatabase(app);
+const db = getFirestore(app);
 
 // Save user data to Firestore
-function saveUserData(userId, userData) {
-    set(ref(db, 'users/' + userId), userData);
+async function saveUserData(userId, userData) {
+    try {
+        const docRef = doc(db, 'users', userId);
+        await setDoc(docRef, userData);
+    } catch (e){
+        console.log(e)
+    }
 }
 
 function Home() {
@@ -35,17 +40,17 @@ function Home() {
     ];
 
     const handleLogout = async () => {
-      try {
-        navigation.navigate('Login');
-        await AsyncStorage.removeItem('accessToken');
-        await AsyncStorage.removeItem('refreshToken');
-        await AsyncStorage.removeItem('expirationTime');    
-        console.log('Logout successful');
-      } catch (error) {
-        console.error('Error during logout:', error);
-      }
+        try {
+            navigation.navigate('Login');
+            await AsyncStorage.removeItem('accessToken');
+            await AsyncStorage.removeItem('refreshToken');
+            await AsyncStorage.removeItem('expirationTime');
+            console.log('Logout successful');
+        } catch (error) {
+            console.error('Error during logout:', error);
+        }
     };
-    
+
     // const handleCreatePlaylist = async () => {
     //     try {
     //         const userId = await AsyncStorage.getItem('userId');
@@ -78,25 +83,26 @@ function Home() {
     //         console.error('Error creating playlist:', error);
     //     }
     // };
-    
+
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const accessToken = await AsyncStorage.getItem('accessToken');
                 const apiUrl = 'https://api.spotify.com/v1/me';
-                
+
                 const response = await fetch(apiUrl, {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
                 });
-        
+
                 if (response.ok) {
                     const userData = await response.json();
                     console.log('User Data:', userData);
                     saveUserData(userData.id, userData);
                     await AsyncStorage.setItem('userId', userData.id);
+                    await AsyncStorage.setItem('userData', JSON.stringify(userData));
                 } else {
                     console.error('Failed to fetch user data:', response.statusText);
                 }
@@ -104,13 +110,13 @@ function Home() {
                 console.error('Error fetching user data:', error);
             }
         };
-        fetchData(); 
-    }, []); 
+        fetchData();
+    }, []);
 
     return (
         <View style={styles.container}>
             <View style={styles.profileContainer}>
-            {/* GET SPOTIFY PROFILE PIC 
+                {/* GET SPOTIFY PROFILE PIC 
             <Image
                 source={require('./path_to_your_image/profile_pic.png')} 
                 style={styles.profilePic}
@@ -128,15 +134,15 @@ function Home() {
                 data={tempos}
                 showsHorizontalScrollIndicator={false}
                 renderItem={({ item }) => (
-                <View style={styles.itemContainer}>                
-                    <View style={[styles.rectangle]}>
-                        <Text style={[styles.tempoText]}>{item.name}</Text>
+                    <View style={styles.itemContainer}>
+                        <View style={[styles.rectangle]}>
+                            <Text style={[styles.tempoText]}>{item.name}</Text>
 
-                        <TouchableOpacity style={styles.playButton}>
+                            <TouchableOpacity style={styles.playButton}>
                                 <Text style={styles.playButtonText}>Listen</Text>
-                        </TouchableOpacity>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                </View>
                 )}
                 keyExtractor={(tempo) => tempo.id}
             />
@@ -152,28 +158,28 @@ function Home() {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      alignItems: 'flex-start',
-      justifyContent: 'flex-start',
-      padding: 20,
-      backgroundColor: '#14333F',
+        flex: 1,
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        padding: 20,
+        backgroundColor: '#14333F',
     },
     profileContainer: {
         width: 50,
         height: 50,
-        borderRadius: 50, 
+        borderRadius: 50,
         backgroundColor: "white",
         marginBottom: 70,
     },
     profilePic: {
-      width: 50,
-      height: 50,
-      borderRadius: 25,
+        width: 50,
+        height: 50,
+        borderRadius: 25,
     },
     text: {
-        color: 'white', 
-        fontSize: 16, 
-        fontWeight: 'bold', 
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
         marginBottom: 20,
     },
     itemContainer: {
@@ -191,8 +197,8 @@ const styles = StyleSheet.create({
         marginBottom: 10,
     },
     tempoText: {
-        color: 'black', 
-        fontSize: 16, 
+        color: 'black',
+        fontSize: 16,
         padding: 15,
     },
     playButton: {
@@ -207,17 +213,17 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     buttonContainer: {
-      backgroundColor: '#4C7F7E', 
-      width: '100%',
-      paddingVertical: 15,
-      alignItems: 'center',
-      borderRadius: 10,
+        backgroundColor: '#4C7F7E',
+        width: '100%',
+        paddingVertical: 15,
+        alignItems: 'center',
+        borderRadius: 10,
     },
     buttonText: {
-        color: 'white', 
-        fontSize: 16, 
-        fontWeight: 'bold', 
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
     },
-  });
-  
-  export default Home;
+});
+
+export default Home;
